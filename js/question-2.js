4fc46e5daa28137a08df92368947011a000999ed
Isabel Maria Guner-Velasco <isabelmariaoboe@gmail.com>
2022-02-12 13:37:19 +0100

    handling errors

--- js/question-2.js
+++ js/question-2.js
@@ -1,21 +1,40 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordening=-rating&key=80168c6cc0ef43d5a0fc60b06dc0d6db"
 
-async function getGames () {
-    // fetch
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+    // handling errors with try/catch
+    try {
+        // fetch
 
     const response = await fetch(url);
-
     const results = await response.json();
-
     const games = results.results;
 
     //console.log(results.results);
     // loop
 
-    for(let i = 0; i < games.length; i++){
-        console.log(games[i].name);
-        console.log(games[i].rating);
-    }
-}
+    resultsContainer.innerHTML = " ";
 
-getGames ();
+        for(let i = 0; i < games.length; i++){
+            var currentGame = games[i];
+            console.log(currentGame.name, currentGame.rating);
+            if (i === 8) {
+                break;
+            }
+            // adding the results to the HTML
+            resultsContainer.innerHTML += `<div class="result">${currentGame.name + ": " + currentGame.rating}` 
+        }
+    }
+
+    catch (error) {
+        console.log("An error occurred")
+        resultsContainer.innerHTML = displayError("An error occurred");
+    }
+    
+    
+} 
+
+getGames()
+    
+
